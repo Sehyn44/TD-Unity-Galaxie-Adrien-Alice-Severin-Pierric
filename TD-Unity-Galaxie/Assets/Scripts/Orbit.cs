f9875e8691c67a1e53346ce4a109af705f514f07
Sehyn44 <149390442+Sehyn44@users.noreply.github.com>
2025-11-05 14:40:26 +0100

    add planetb, orbit script, and texture for sun and planetb

--- TD-Unity-Galaxie/Assets/Scripts/Orbit.cs
+++ TD-Unity-Galaxie/Assets/Scripts/Orbit.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Orbit : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    //Centre de l’orbite (ex: le Soleil)
+    public Transform pivot;
+    //Vitesse angulaire en degrés/seconde
+    public float angularSpeed = 20f;
+    //Rotation propre de la planète en degrés/seconde
+    public float selfRotationSpeed = 30f;
     void Update()
     {
-        
+        if (pivot != null)
+        {
+            // orbite autour du pivot (axe Y)
+            transform.RotateAround(pivot.position, Vector3.up, angularSpeed * Time.deltaTime);
+        }
+        // rotation sur soi-même (axe Y)
+        transform.Rotate(Vector3.up, selfRotationSpeed * Time.deltaTime, Space.Self);
     }
 }
